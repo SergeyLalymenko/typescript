e86f7aed2171a86274f14f7c25a66970b6af5623
Sergey Lalymenko <sergeylalymenko@doubledmarketing.com>
2023-12-28 19:35:25 +0200

    [development] hw4

--- hw4/src/index.js
+++ hw4/src/index.js
@@ -1,3 +1,12 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 var calculator = {
     add: function (a, b) { return a + b; },
     subtract: function (a, b) { return a - b; },
@@ -7,4 +16,89 @@ var calculator = {
 function calculate(calculator, operation, operands) {
     return calculator[operation].apply(calculator, operands);
 }
-console.log(calculate(calculator, 'divide', [6, 2]));
+var BookService = /** @class */ (function () {
+    function BookService(authors, books) {
+        this.authors = authors;
+        this.books = books;
+    }
+    BookService.prototype.addAuthor = function (author) {
+        var isAuthor = this.getIsAuthorById(author.id);
+        if (isAuthor)
+            return;
+        this.authors = __spreadArray(__spreadArray([], this.authors, true), [author], false);
+    };
+    BookService.prototype.addBook = function (book) {
+        var isAuthor = this.getIsAuthorById(book.author.id);
+        if (!isAuthor) {
+            this.addAuthor(book.author);
+        }
+        this.books = __spreadArray(__spreadArray([], this.books, true), [book], false);
+    };
+    BookService.prototype.getIsAuthorById = function (id) {
+        return !!this.authors.find(function (author) { return author.id === id; });
+    };
+    BookService.prototype.getAuthorById = function (id) {
+        return this.authors.find(function (author) { return author.id === id; });
+    };
+    BookService.prototype.getBookById = function (id) {
+        return this.books.find(function (book) { return book.id === id; });
+    };
+    BookService.prototype.getBooksByAuthorId = function (id) {
+        return this.books.filter(function (_a) {
+            var author = _a.author;
+            return author.id === id;
+        });
+    };
+    BookService.prototype.removeBooksByAuthorId = function (id) {
+        var isAuthor = this.getIsAuthorById(id);
+        if (!isAuthor)
+            return;
+        this.books = this.books.filter(function (book) { return book.author.id !== id; });
+    };
+    BookService.prototype.removeAuthorById = function (id) {
+        var isAuthor = this.getIsAuthorById(id);
+        if (!isAuthor)
+            return;
+        this.removeBooksByAuthorId(id);
+        this.authors = this.authors.filter(function (author) { return author.id !== id; });
+    };
+    BookService.prototype.removeBookById = function (id) {
+        this.books = this.books.filter(function (book) { return book.id !== id; });
+    };
+    return BookService;
+}());
+var bookService = new BookService([
+    {
+        id: 1,
+        name: 'author name1',
+    },
+    {
+        id: 2,
+        name: 'author name2',
+    },
+], [
+    {
+        title: 'book title1',
+        id: 1,
+        author: {
+            id: 1,
+            name: 'author name1',
+        },
+    },
+    {
+        title: 'book title2',
+        id: 2,
+        author: {
+            id: 1,
+            name: 'author name1',
+        },
+    },
+    {
+        title: 'book title3',
+        id: 3,
+        author: {
+            id: 2,
+            name: 'author name2',
+        },
+    },
+]);
--- hw4/src/index.ts
+++ hw4/src/index.ts
@@ -18,4 +18,130 @@ function calculate(calculator: ICalculator, operation: keyof ICalculator, operan
     return calculator[operation](...operands);
 }
 
-console.log(calculate(calculator, 'divide', [6, 0]));
+
+
+interface IAuthor {
+    id: number;
+    name: string;
+}
+
+interface IBook {
+    title: string;
+    author: IAuthor;
+    id: number;
+}
+
+interface IBookService {
+    authors: IAuthor[];
+    books: IBook[];
+    addAuthor(author: IAuthor): void;
+    addBook(book: IBook): void;
+    getIsAuthorById(id: IAuthor['id']): boolean;
+    getAuthorById(id: IAuthor['id']): IAuthor | undefined;
+    getBookById(id: IBook['id']): IBook | undefined;
+    getBooksByAuthorId(id: IAuthor['id']): IBook[];
+    removeBooksByAuthorId(id: IAuthor['id']): void;
+    removeAuthorById(id: IAuthor['id']): void;
+    removeBookById(id: IBook['id']): void;
+}
+
+class BookService implements IBookService {
+    authors;
+    books;
+
+    constructor(authors: IAuthor[], books: IBook[]) {
+        this.authors = authors;
+        this.books = books;
+    }
+
+    addAuthor(author: IAuthor): void { // почему если не прописать тип IAuthor, то тс не понимает что это за тип? В интерфейсе же описано что сюда приходит IAuthor
+        const isAuthor: boolean = this.getIsAuthorById(author.id);
+        if (isAuthor) return;
+
+        this.authors = [...this.authors, author];
+    }
+
+    addBook(book: IBook): void {
+        const isAuthor: boolean = this.getIsAuthorById(book.author.id);
+
+        if (!isAuthor) {
+            this.addAuthor(book.author);
+        }
+
+        this.books = [...this.books, book];
+    }
+
+    getIsAuthorById(id: IAuthor['id']): boolean {
+        return !!this.authors.find((author) => author.id === id);
+    }
+
+    getAuthorById(id: IAuthor['id']): IAuthor | undefined {
+        return this.authors.find((author) => author.id === id);
+    }
+
+    getBookById(id: IBook['id']): IBook | undefined {
+        return this.books.find((book) => book.id === id);
+    }
+
+    getBooksByAuthorId(id: IAuthor['id']): IBook[] {
+        return this.books.filter(({ author }) => author.id === id);
+    }
+
+    removeBooksByAuthorId(id: IAuthor['id']): void {
+        const isAuthor: boolean = this.getIsAuthorById(id);
+        if (!isAuthor) return;
+
+        this.books = this.books.filter((book) => book.author.id !== id);
+    }
+
+    removeAuthorById(id: IAuthor['id']): void {
+        const isAuthor: boolean = this.getIsAuthorById(id);
+        if (!isAuthor) return;
+
+        this.removeBooksByAuthorId(id);
+        this.authors = this.authors.filter((author) => author.id !== id);
+    }
+
+    removeBookById(id: IBook['id']): void {
+        this.books = this.books.filter((book) => book.id !== id);
+    }
+}
+
+const bookService: BookService = new BookService(
+    [
+        {
+            id: 1,
+            name: 'author name1',
+        },
+        {
+            id: 2,
+            name: 'author name2',
+        },
+    ],
+    [
+        {
+            title: 'book title1',
+            id: 1,
+            author: {
+                id: 1,
+                name: 'author name1',
+            },
+        },
+        {
+            title: 'book title2',
+            id: 2,
+            author: {
+                id: 1,
+                name: 'author name1',
+            },
+        },
+        {
+            title: 'book title3',
+            id: 3,
+            author: {
+                id: 2,
+                name: 'author name2',
+            },
+        },
+    ],
+);
